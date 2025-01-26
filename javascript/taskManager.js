bd05661aad0f37911d795d295f42c5aef6b8a1a1
mdselimme <mdselimdev@gmail.com>
2025-01-26 18:01:49 +0600

    add to task function and find value

--- javascript/taskManager.js
+++ javascript/taskManager.js
@@ -1,1 +1,9 @@
 console.log("Task Manager");
+
+const addTask = (event) => {
+  event.preventDefault();
+  const taskName = event.target.taskTitle.value;
+  const taskDescription = event.target.taskDescription.value;
+  const taskPriority = event.target.taskPriority.value;
+  console.log(taskName, taskDescription, taskPriority);
+};
