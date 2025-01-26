99a2f573bfc697ff5fe3199198c6ee0ccf433276
mdselimme <mdselimdev@gmail.com>
2025-01-26 19:49:57 +0600

    show data on the display

--- javascript/taskManager.js
+++ javascript/taskManager.js
@@ -1,5 +1,6 @@
 console.log("Task Manager");
 
+// Find Input Data From Data Input
 const addTask = (event) => {
   event.preventDefault();
   const taskName = event.target.taskTitle.value;
@@ -13,8 +14,10 @@ const addTask = (event) => {
   addToTaskDb(data);
   const getData = getDataFromDb();
   showDataOnTheWeb(getData);
+  event.target.reset();
 };
 
+// Get data from localStorage database
 const getDataFromDb = () => {
   const getTask = localStorage.getItem("tasks");
   if (getTask) {
@@ -23,13 +26,35 @@ const getDataFromDb = () => {
   return [];
 };
 
+// Display data on the website
 const showDataOnTheWeb = (data) => {
-  console.log(data);
+  const taskList = document.getElementById("taskList");
+  taskList.textContent = "";
+  data.forEach((element) => {
+    const div = document.createElement("div");
+    div.classList.add("task-item");
+    div.innerHTML = ` 
+      <p>${element.uniqueId}</p>
+      <p>${element.taskName}</p>
+      <p>${element.taskDescription}</p>
+      <p>${element.taskPriority}</p>
+      <div class="task-actions">
+      <button class="complete-btn">Complete</button>
+      <button class="delete-btn">Delete</button>
+      </div>
+    `;
+    taskList.appendChild(div);
+  });
+  console.log(data, taskList);
 };
 
+// -- Add task in the localStorage database --
 const addToTaskDb = (task) => {
   const result = getDataFromDb();
   const setTask = [...result, task];
   const parseData = JSON.stringify(setTask);
   localStorage.setItem("tasks", parseData);
 };
+
+const getData = getDataFromDb();
+showDataOnTheWeb(getData);
