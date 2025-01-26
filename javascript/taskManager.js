d8c93b6646df1bebb34d1c8f34e420f45069eaf5
mdselimme <mdselimdev@gmail.com>
2025-01-26 20:38:29 +0600

    add sweetalert and delete data

--- javascript/taskManager.js
+++ javascript/taskManager.js
@@ -6,11 +6,8 @@ const addTask = (event) => {
   const taskName = event.target.taskTitle.value;
   const taskDescription = event.target.taskDescription.value;
   const taskPriority = event.target.taskPriority.value;
-  console.log(taskName, taskDescription, taskPriority);
   const uniqueId = new Date().getTime().toString();
-  console.log(uniqueId);
   const data = { uniqueId, taskName, taskDescription, taskPriority };
-  console.log(data);
   addToTaskDb(data);
   const getData = getDataFromDb();
   showDataOnTheWeb(getData);
@@ -40,21 +37,67 @@ const showDataOnTheWeb = (data) => {
       <p>${element.taskPriority}</p>
       <div class="task-actions">
       <button class="complete-btn">Complete</button>
-      <button class="delete-btn">Delete</button>
+      <button onclick="deleteTaskFromDb(${element.uniqueId})" class="delete-btn">Delete</button>
       </div>
     `;
     taskList.appendChild(div);
   });
-  console.log(data, taskList);
+};
+
+const saveToLocalStorage = (data) => {
+  const parseData = JSON.stringify(data);
+  localStorage.setItem("tasks", parseData);
 };
 
 // -- Add task in the localStorage database --
 const addToTaskDb = (task) => {
   const result = getDataFromDb();
   const setTask = [...result, task];
-  const parseData = JSON.stringify(setTask);
-  localStorage.setItem("tasks", parseData);
+  saveToLocalStorage(setTask);
+  Swal.fire({
+    icon: "success",
+    title: "Add Task Successfully",
+    showConfirmButton: false,
+    timer: 1500,
+  });
 };
 
-const getData = getDataFromDb();
-showDataOnTheWeb(getData);
+const deleteTaskFromDb = (task) => {
+  Swal.fire({
+    title: "Are you sure ? ",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      const allTask = getDataFromDb();
+      allTask.forEach((err) => {
+        console.log(err.uniqueId);
+      });
+      const deleteTask = allTask.filter(
+        (item) => parseInt(item.uniqueId) !== task
+      );
+      saveToLocalStorage(deleteTask);
+      Swal.fire({
+        title: "Deleted!",
+        text: "Your file has been deleted.",
+        icon: "success",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      calledData();
+    }
+  });
+
+  console.log("delete", deleteTask);
+};
+
+const calledData = () => {
+  const getData = getDataFromDb();
+  showDataOnTheWeb(getData);
+};
+
+calledData();
