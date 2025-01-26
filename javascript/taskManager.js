11edeb8f4e8e932126404984c60e6754165d291c
mdselimme <mdselimdev@gmail.com>
2025-01-26 19:24:08 +0600

    save data in the localstorage

--- javascript/taskManager.js
+++ javascript/taskManager.js
@@ -6,4 +6,30 @@ const addTask = (event) => {
   const taskDescription = event.target.taskDescription.value;
   const taskPriority = event.target.taskPriority.value;
   console.log(taskName, taskDescription, taskPriority);
+  const uniqueId = new Date().getTime().toString();
+  console.log(uniqueId);
+  const data = { uniqueId, taskName, taskDescription, taskPriority };
+  console.log(data);
+  addToTaskDb(data);
+  const getData = getDataFromDb();
+  showDataOnTheWeb(getData);
+};
+
+const getDataFromDb = () => {
+  const getTask = localStorage.getItem("tasks");
+  if (getTask) {
+    return JSON.parse(getTask);
+  }
+  return [];
+};
+
+const showDataOnTheWeb = (data) => {
+  console.log(data);
+};
+
+const addToTaskDb = (task) => {
+  const result = getDataFromDb();
+  const setTask = [...result, task];
+  const parseData = JSON.stringify(setTask);
+  localStorage.setItem("tasks", parseData);
 };
