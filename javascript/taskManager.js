49366ea1c7b119fb052e75b89565f68276265c3e
mdselimme <mdselimdev@gmail.com>
2025-01-27 17:39:11 +0600

    add-update-task-manager-functionality

--- javascript/taskManager.js
+++ javascript/taskManager.js
@@ -1,6 +1,4 @@
-console.log("Task Manager");
-
-// Get data from localStorage database
+// Get data from Json Data from localStorage database
 const getDataFromDb = () => {
   const getTask = localStorage.getItem("tasks");
   if (getTask) {
@@ -9,7 +7,7 @@ const getDataFromDb = () => {
   return [];
 };
 
-// Find Input Data From Data Input
+// Find Input Data From Data Input Add to task
 const addTask = (event) => {
   event.preventDefault();
   const taskName = event.target.taskTitle.value;
@@ -23,29 +21,36 @@ const addTask = (event) => {
   event.target.reset();
 };
 
-// Display data on the website
+// Display data on the webpage
 const showDataOnTheWeb = (data) => {
   const taskList = document.getElementById("taskList");
+  const taskHead = document.getElementById("taskHead");
   taskList.textContent = "";
-  data.forEach((element) => {
-    const div = document.createElement("div");
-    div.classList.add("task-item");
-    div.innerHTML = ` 
+  if (data.length === 0) {
+    taskHead.style.display = "none";
+    taskList.innerHTML = `<h1 class="show-no-task">No Task Added</h1>`;
+  } else {
+    taskHead.style.display = "block";
+    data.forEach((element) => {
+      const div = document.createElement("div");
+      div.classList.add("task-item");
+      div.innerHTML = ` 
       <p>${element.uniqueId}</p>
       <p>${element.taskName}</p>
       <p>${element.taskDescription}</p>
       <p>${element.taskPriority}</p>
       <div class="task-actions">
       <button class="complete-btn">Complete</button>
-      <button onclick="updateData(${element.uniqueId})" class="update-btn">Update</button>
+      <button onclick="updateDataDisplay(${element.uniqueId})" class="update-btn">Update</button>
       <button onclick="deleteTaskFromDb(${element.uniqueId})" class="delete-btn">Delete</button>
       </div>
     `;
-    taskList.appendChild(div);
-  });
+      taskList.appendChild(div);
+    });
+  }
 };
 
-// Save Data To Local Storage
+// Save Json Data To Local Storage
 const saveToLocalStorage = (data) => {
   const parseData = JSON.stringify(data);
   localStorage.setItem("tasks", parseData);
@@ -95,25 +100,17 @@ const deleteTaskFromDb = (task) => {
 };
 
 // Find Task Data By Id
-// const findUniqueIdData = (id) => {
-document.getElementById("UpdateToTaskBox").style.display = "block";
-document.getElementById("addToTaskBox").style.display = "none";
-//   const allData = getDataFromDb();
-//   const findData = allData.find((ele) => parseInt(ele.uniqueId) === id);
-//   const upTaskTitle = document.getElementById("updateTaskTitle");
-//   const upTaskDescription = document.getElementById("updateTaskDescription");
-//   const updateTaskPriority = document.getElementById("updateTaskPriority");
-//   upTaskTitle.value = findData.taskName;
-//   upTaskDescription.value = findData.taskDescription;
-//   updateTaskPriority.value = findData.taskPriority;
-// };
-
-const updateData = (id) => {
-  // findUniqueIdData(id);
+const findUniqueIdData = (id) => {
   const allData = getDataFromDb();
+  const findData = allData.find((ele) => parseInt(ele.uniqueId) === id);
+  return findData;
+};
+
+// Update Display Form Data Show
+const updateDataDisplay = (id) => {
+  const findData = findUniqueIdData(id);
   document.getElementById("UpdateToTaskBox").style.display = "block";
   document.getElementById("addToTaskBox").style.display = "none";
-  const findData = allData.find((ele) => parseInt(ele.uniqueId) === id);
   const uniqueId = document.getElementById("uniqueId");
   const upTaskTitle = document.getElementById("updateTaskTitle");
   const upTaskDescription = document.getElementById("updateTaskDescription");
@@ -122,28 +119,28 @@ const updateData = (id) => {
   upTaskTitle.value = findData.taskName;
   upTaskDescription.value = findData.taskDescription;
   updateTaskPriority.value = findData.taskPriority;
-  // document.getElementById("updateTaskTitle").value = findData.taskName;
-  // document.getElementById("updateTaskDescription").value =
-  //   findData.taskDescription;
-  // document.getElementById("updateTaskPriority").value = findData.taskPriority;
-  // showUpdateDataInInput(findData);
 };
 
-// const showUpdateDataInInput = (tasksData) => {
-//   console.log("showdata = ", tasksData);
-//   document.getElementById("updateTaskTitle").value = tasksData.taskName;
-//   document.getElementById("updateTaskDescription").value =
-//     tasksData.taskDescription;
-//   document.getElementById("updateTaskPriority").value = tasksData.taskPriority;
-// };
-
+// Update Form Find Data and Update the Specific Element Of the task list and show Alert
 const UpdateTaskForm = (event) => {
   event.preventDefault();
+  const allData = getDataFromDb();
   const uniqueId = event.target.uniqueId.value;
   const taskName = event.target.taskTitle.value;
   const taskDescription = event.target.taskDescription.value;
   const taskPriority = event.target.taskPriority.value;
-  console.log(uniqueId, taskName, taskDescription, taskPriority);
+  const index = allData.findIndex((data) => data.uniqueId === uniqueId);
+  allData[index] = { uniqueId, taskName, taskDescription, taskPriority };
+  saveToLocalStorage(allData);
+  calledData();
+  Swal.fire({
+    icon: "success",
+    title: "Task Update Successfully",
+    showConfirmButton: false,
+    timer: 1500,
+  });
+  document.getElementById("UpdateToTaskBox").style.display = "none";
+  document.getElementById("addToTaskBox").style.display = "block";
 };
 
 // Default Called Data form Db
@@ -151,5 +148,3 @@ const calledData = () => {
   const getData = getDataFromDb();
   showDataOnTheWeb(getData);
 };
-
-calledData();
